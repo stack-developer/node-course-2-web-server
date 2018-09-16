341e06936ec208265dbaba06e4d7b71bf6010055
Harish Chaudhary <imstackdeveloper@gmail.com>
2018-09-16 15:54:07 +0530

    New project pahe added and changes in server.js and header.hbs

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/project', (req, res)=>{
+  res.render('project.hbs',{
+    pageTitle:"Project"
+  })
+})
+
 app.get('/about', (req, res) =>{
   res.render('about.hbs',{
     pageTitle : "About Page"
